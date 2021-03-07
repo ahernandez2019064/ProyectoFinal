c50ae67ccd44784aaa0055597ba7e4921f954a60
Allan Hernandez <ahernandez-2019064@kinal.edu.gt>
2021-03-06 19:53:36 -0600

    agregar variables de ruta

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const bodyparser = require('body-parser');
 
 //Importaciones
 var usuario_route = require('./src/rutas/usuarioRutas');
+var categoria_route = require('./src/rutas/categoriaRutas');
+var producto_route = require('./src/rutas/productoRutas');
 
 //MIDDLEWARE
 app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
-app.use('/api', usuario_route);
+app.use('/api', usuario_route, categoria_route, producto_route);
 
 //Exportar
 module.exports = app;
