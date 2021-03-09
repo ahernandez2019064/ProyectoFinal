77d57bac24dc0dcda146f7106525444e812914e1
Allan Hernandez <ahernandez-2019064@kinal.edu.gt>
2021-03-08 19:29:21 -0600

    Agregar rutas de las funciones creadas

--- src/rutas/categoriaRutas.js
+++ src/rutas/categoriaRutas.js
@@ -10,5 +10,6 @@ var app = express.Router();
 app.post('/agregarCategoria', md_autorizacion.ensureAuth, categoriaControlador.agregarCategoria);
 app.put('/editarCategoria/:id', md_autorizacion.ensureAuth, categoriaControlador.editarCategoria);
 app.get('/listarCategorias', md_autorizacion.ensureAuth, categoriaControlador.listarCategorias);
+app.delete('/eliminarCategoria/:id', md_autorizacion.ensureAuth, categoriaControlador.eliminarCategoria);
 
 module.exports = app;
--- src/rutas/productoRutas.js
+++ src/rutas/productoRutas.js
@@ -10,5 +10,7 @@ var app = express.Router();
 app.post('/agregarProducto/:id', md_autorizacion.ensureAuth, productoControlador.agregarProducto);
 app.put('/editarProducto/:id', md_autorizacion.ensureAuth, productoControlador.editarProducto);
 app.delete('/eliminarProducto/:id', md_autorizacion.ensureAuth, productoControlador.eliminarProducto);
+app.get('/listarProductos', md_autorizacion.ensureAuth, productoControlador.listarProductos);
+app.get('/obtenerxId/:id', md_autorizacion.ensureAuth, productoControlador.obtenerProductoId);
 
 module.exports = app;
