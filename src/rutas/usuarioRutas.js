8bc79618a497b8846f131464b1e97f98d7410e81
Allan Hernandez <ahernandez-2019064@kinal.edu.gt>
2021-03-09 00:25:27 -0600

    Añadir las rutas correspondientes a sus funciones

--- src/rutas/usuarioRutas.js
+++ src/rutas/usuarioRutas.js
@@ -7,11 +7,17 @@ var md_autorizacion = require('../middleware/authentication');
 
 var app = express.Router();
 
+//Rutas Administrador
 app.post('/login', usuarioControlador.login);
-app.post('/registrarCliente', usuarioControlador.registrarCliente);
 app.post('/registrarAdmin', md_autorizacion.ensureAuth ,usuarioControlador.registrarAdmin);
 app.delete('/eliminarUsuario/:id', md_autorizacion.ensureAuth, usuarioControlador.eliminarUsuario);
 app.put('/editarUsuario/:id', md_autorizacion.ensureAuth, usuarioControlador.editarUsuario);
 app.put('/ascenderUsuario/:id', md_autorizacion.ensureAuth, usuarioControlador.ascenderUsuario);
+
+// Rutas Clientes
+app.delete('/eliminarCuenta/:id', md_autorizacion.ensureAuth, usuarioControlador.eliminarCuenta);
+app.put('/editarCuenta/:id', md_autorizacion.ensureAuth, usuarioControlador.editarCuenta);
+app.post('/registrarCliente', usuarioControlador.registrarCliente);
+app.put('/agregarAlCarrito/:id', md_autorizacion.ensureAuth, usuarioControlador.agregarProductoCarrito);
 
 module.exports = app;
