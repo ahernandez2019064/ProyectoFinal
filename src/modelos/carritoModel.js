fd08ac269b9729f6cd0c5bc056b3b323b384cfef
Allan Hernandez <ahernandez-2019064@kinal.edu.gt>
2021-03-08 19:27:01 -0600

    Creacion del modelo de carrito

---
+++ src/modelos/carritoModel.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var CarritoSchema = Schema({
+    listaProductos:[{
+        cantidad: Number,
+        subTotal: Number,
+        producto: {type: Schema.ObjectId, ref: 'productos'}
+    }],
+    carritoUsuario: {type: Schema.Types.ObjectId, ref: 'usuarios'}
+})
+
+module.exports = mongoose.model('carritos', CarritoSchema);
