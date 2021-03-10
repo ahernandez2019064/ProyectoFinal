c244fe48ed1d37c4ac23aea36f14ae4ac8ac38ad
Allan Hernandez <ahernandez-2019064@kinal.edu.gt>
2021-03-09 22:34:19 -0600

    Creacion de modelo factura

---
+++ src/modelos/facturaModel.js
@@ -0,0 +1,16 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var FacturaSchema = Schema ({
+    listaProductos:[{
+        cantidad: Number,
+        subTotal: Number,
+        producto: {type: Schema.ObjectId, ref: 'productos'}
+    }],
+    facturaUsuario: {type: Schema.Types.ObjectId, ref: 'usuarios'},
+    total: Number
+});
+
+module.exports = mongoose.model('facturas', FacturaSchema);
